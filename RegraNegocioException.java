c327be136dd988f416863d3aafafe31188317ded
Marcosil5364 <marcossilva5364@outlook.com>
2022-12-26 09:23:41 -0300

    Add files via upload

---
+++ RegraNegocioException.java
@@ -0,0 +1,10 @@
+package com.marsilva.minhasfinancas.exception;
+
+public class RegraNegocioException extends RuntimeException {
+	
+	public RegraNegocioException(String msg) {
+		
+		super(msg);
+	}
+
+}
